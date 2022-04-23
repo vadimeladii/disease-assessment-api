e7aec317861cd60dcf01e37a48dcfd01f9d7ea75
vadimeladii <veladii@straighterline.com>
2022-04-23 19:49:57 +0300

    Init spring boot project

---
+++ src/main/java/md/utm/is/DiseaseAssessmentApiApplication.java
@@ -0,0 +1,13 @@
+package md.utm.is;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseaseAssessmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiseaseAssessmentApiApplication.class, args);
+    }
+
+}
